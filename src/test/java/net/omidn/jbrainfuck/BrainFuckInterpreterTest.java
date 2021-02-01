b90b8b9bf9a70894cdf616e03af9f0628445a098
Omid.N <nejadabbasio@gmail.com>
2021-02-02 01:15:05 +0330

    Added a test cade for removeComments() method of BrainFuckInterpreter

--- src/test/java/net/omidn/jbrainfuck/BrainFuckInterpreterTest.java
+++ src/test/java/net/omidn/jbrainfuck/BrainFuckInterpreterTest.java
@@ -1,12 +1,31 @@
 package net.omidn.jbrainfuck;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 import org.junit.Test;
 
 public class BrainFuckInterpreterTest {
 
 	@Test
-	public void testRemoveComments() {
+	public void testRemoveComments() throws Exception {
+		String bfCode = getResourceAsString("helloworld.bf");
+		String helloworld = "[]++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+		assertEquals(helloworld, BrainFuckInterpreter.removeComments(bfCode));
 		
+		
+	}
+
+	private String getResourceAsString(String resName) throws IOException, URISyntaxException {
+		File file = new File(getClass().getClassLoader().getResource("helloworld.bf").toURI());
+		BufferedInputStream bf = new BufferedInputStream(new FileInputStream(file));
+		String str = new String(bf.readAllBytes());
+		return str;
 	}
 	
 }
