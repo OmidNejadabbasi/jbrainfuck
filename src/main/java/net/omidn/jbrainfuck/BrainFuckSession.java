633e0c736ad6649d62e095086830da74586aa897
Omid.N <nejadabbasio@gmail.com>
2021-02-01 13:14:42 +0330

    Added a comprehensive constructore method to BrainFuckSession

--- src/main/java/net/omidn/jbrainfuck/BrainFuckSession.java
+++ src/main/java/net/omidn/jbrainfuck/BrainFuckSession.java
@@ -11,5 +11,15 @@ public class BrainFuckSession {
 	private InputStream stdin;
 	private OutputStream stdout;
 	
+	public BrainFuckSession(int intitialCapacity, InputStream stdin, OutputStream stdout) {
+
+		this.pointer = 0;
+		charList = new ArrayList<Character>(intitialCapacity);
+		this.stdin = stdin;
+		this.stdout = stdout;
+
+	}
+	
+	
 	
 }
