76d906c78cac1b5920be102bc4bc6faaa128c0b5
Omid.N <nejadabbasio@gmail.com>
2021-02-01 15:34:37 +0330

    Added a constructore to BrainFuckInterpreter that accepts java.io.File

--- src/main/java/net/omidn/jbrainfuck/BrainFuckInterpreter.java
+++ src/main/java/net/omidn/jbrainfuck/BrainFuckInterpreter.java
@@ -1,10 +1,26 @@
 package net.omidn.jbrainfuck;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
 public class BrainFuckInterpreter {
 	private String bfCode;
 	
 	public BrainFuckInterpreter(String bfCode) {
 		this.bfCode = bfCode;
+	}
+	
+	public BrainFuckInterpreter(File bfFile) throws IOException {
+		StringBuilder sb = new StringBuilder();
+
+		List<String> allLines = Files.readAllLines(bfFile.toPath());
+
+		for (String s : allLines) {
+			sb.append(s);
+		}
+		this.bfCode = sb.toString();
 	}
 	
 }
